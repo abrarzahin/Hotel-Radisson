8a093bd449c4980841105186d7755a7cd239e749
abrar zahin <abrarzahin@yahoo.com>
2019-07-14 18:36:51 -0400

    22 angular reactive form using form group and form control

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -14,8 +14,31 @@ export class AppComponent {
   constructor(private reservationService: ReservationService){}
 
   rooms: Room[];
+  roomSearchForm: FormGroup;
+  currentCheckInVal: string;
+  currentCheckOutVal: string;
+  currentPrice: number;
+  currentRoomNumber: number;
 
   ngOnInit() {
+    this.roomSearchForm = new FormGroup ({
+      checkin: new FormControl(''),
+      checkout: new FormControl(''),
+      roomNumber: new FormControl('')
+    });
+
+    this.roomSearchForm.valueChanges.subscribe(form=>{
+      this.currentCheckInVal= form.checkin;
+      this.currentCheckOutVal=form.checkout;
+      if(form.roomNumber){
+        let roomValues: string[] = form.roomNumber.split('|');
+        this.currentRoomNumber =Number( roomValues[0]);
+        this.currentPrice=Number(roomValues[1]);
+      }
+      
+    });
+
+
     this.rooms = [ new Room("127", "127", "150"),
     new Room("138", "138", "180"),
     new Room("254", "254", "200")
