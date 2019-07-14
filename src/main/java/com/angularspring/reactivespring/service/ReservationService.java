64067e0d4a337ca9bd975aa2ea86a82ade2fb582
abrar zahin <abrarzahin@yahoo.com>
2019-07-14 11:45:58 -0400

    15 service interface

---
+++ src/main/java/com/angularspring/reactivespring/service/ReservationService.java
@@ -0,0 +1,15 @@
+package com.angularspring.reactivespring.service;
+
+import com.angularspring.reactivespring.model.Reservation;
+import reactor.core.publisher.Mono;
+
+public interface ReservationService {
+
+    Mono<Reservation> getReservation(String id);
+
+    Mono<Reservation> createReservation(Mono<Reservation> reservationMono);
+
+    Mono<Reservation> updateReservation(String id, Mono<Reservation> reservationMono);
+
+    Mono<Boolean> deleteReservation(String id);
+}
